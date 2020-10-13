380fe961de44e4589ab3e23dfb768a165dba7482
Logan Coates <coates.logan1125@gmail.com>
2020-10-13 17:45:24 -0600

    worked on the js

--- script.js
+++ script.js
@@ -19,4 +19,30 @@ $(document).ready(function() {
 
   let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
 
+  if (test) { console.log(storedPlans); }
+
+  if (storedPlans !== null) {
+    planTextArr = storedPlans;
+  } else {
+    planTextArr = new Array(9);
+    planTextArr[4] = "Lunch date!";
+  }
+
+  if (test) { console.log("full array of plan text",planTextArr); }
+
+  let $plannerDiv = $('#plannerContainer');
+  $plannerDiv.empty();
+
+  if (test) { console.log("current time",nowHour12); }
+
+  for (let hour = 9; hour <= 17; hour++) {
+
+    let index = hour -9;
+
+    let $rowDiv = $('<div>');
+    $rowDiv.addClass('row');
+    $rowDiv.addClass('plannerRow');
+    $rowDiv.attr('hour-index',hour);
+  }
+
 })
