ee716dfc45d5667ee626e52a56165da0a49878b8
Logan Coates <coates.logan1125@gmail.com>
2020-10-14 12:22:03 -0600

    worked on timer

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-$(document).ready(function() {
-  
+$(document).ready(function () {
   var test = false;
 
-  var now = moment().format('MMMM Do YYYY');
+  var now = moment().format("MMMM Do YYYY");
 
-  let nowHour24 = moment().format('H');
-  let nowHour12 = moment().format('h');
+  let nowHour24 = moment().format("H");
+  let nowHour12 = moment().format("h");
 
   if (test) {
     nowHour24 = 13;
     nowHour12 = 1;
   }
 
-  let $dateHeading = $('#navbar-subtitle');
+  let $dateHeading = $("#navbar-subtitle");
   $dateHeading.text(now);
 
   var saveIcon = "./images/save-regular.svg";
 
   let storedPlans = JSON.parse(localStorage.getItem("storedPlans"));
 
-  if (test) { console.log(storedPlans); }
+  if (test) {
+    console.log(storedPlans);
+  }
 
   if (storedPlans !== null) {
     planTextArr = storedPlans;
@@ -28,27 +29,30 @@ $(document).ready(function() {
     planTextArr[4] = "Lunch date!";
   }
 
-  if (test) { console.log("full array of plan text",planTextArr); }
+  if (test) {
+    console.log("full array of plan text", planTextArr);
+  }
 
-  let $plannerDiv = $('#plannerContainer');
+  let $plannerDiv = $("#plannerContainer");
   $plannerDiv.empty();
 
-  if (test) { console.log("current time",nowHour12); }
+  if (test) {
+    console.log("current time", nowHour12);
+  }
 
   for (let hour = 9; hour <= 17; hour++) {
+    let index = hour - 9;
 
-    let index = hour -9;
+    let $rowDiv = $("<div>");
+    $rowDiv.addClass("row");
+    $rowDiv.addClass("plannerRow");
+    $rowDiv.attr("hour-index", hour);
 
-    let $rowDiv = $('<div>');
-    $rowDiv.addClass('row');
-    $rowDiv.addClass('plannerRow');
-    $rowDiv.attr('hour-index',hour);
+    let $col12TimeDiv = $("<div>");
+    $col12TimeDiv.addClass("col-md-2");
 
-    let $col12TimeDiv = $('<div>');
-    $col12TimeDiv.addClass('col-md-2');
-
-    var $timeBoxSpn = $('<span>');
-    $timeBoxSpn.attr('class','timeBox');
+    var $timeBoxSpn = $("<span>");
+    $timeBoxSpn.attr("class", "timeBox");
 
     let displayHour = 0;
     let ampm = "";
@@ -65,13 +69,35 @@ $(document).ready(function() {
     $rowDiv.append($col12TimeDiv);
     $col12TimeDiv.append($timeBoxSpn);
 
-    let $dailyPlanSpn = $('<input>');
+    let $dailyPlanSpn = $("<input>");
 
-    $dailyPlanSpn.attr('id', `input-${index}`);
-    $dailyPlanSpn.attr('hour-index',index);
-    $dailyPlanSpn.attr('type','text');
-    $dailyPlanSpn.attr('class','dailyPlan');
-    
-  }
+    $dailyPlanSpn.attr("id", `input-${index}`);
+    $dailyPlanSpn.attr("hour-index", index);
+    $dailyPlanSpn.attr("type", "text");
+    $dailyPlanSpn.attr("class", "dailyPlan");
 
-})
+    $dailyPlanSpn.val(planTextArr[index]);
+
+    let $col9IptDiv = $("<div>");
+    $col9IptDiv.addClass('col-md-1');
+
+    $rowDiv.append($col9IptDiv);
+    $col9IptDiv.append($dailyPlanSpn);
+
+    let $col1SaveDiv = $('<div>');
+    $col1SaveDiv.addClass('col-md-1');
+
+    let $saveBtn = $('<i>');
+    $saveBtn.attr('id', `saveid-${index}`);
+    $saveBtn.attr('save-id',index);
+    $saveBtn.attr('class',"far fa-save saveIcon");
+
+    $rowDiv.append($col1SaveDiv);
+    $col1SaveDiv.append($saveBtn);
+
+    updateRowColor($rowDiv, hour);
+
+    $plannerDiv.append($rowDiv);
+  };
+  
+});
